ca9cdb8037d922af2ece25d80d6103d9ee73c27b
Yoni <me@goldbergyoni.com>
2020-08-09 10:44:54 +0300

    Genric message

--- exercise-testing-basics.js/test/students-submissions/yoni-goldberg.test.js
+++ exercise-testing-basics.js/test/students-submissions/yoni-goldberg.test.js
@@ -1,9 +1,10 @@
 // 🏅 Your mission is to create your first tests here 💜
-// ✅ Whenever you see this icon, there's a TASK for you
+// ✅ - Whenever you see this icon, there's a TASK for you
 // 💡 - This is an ADVICE symbol, it will appear nearby most tasks and help you in fulfilling the tasks
 
-const testHelper = require('./test-helpers')
-const usersService = require('../users-service')
+
+const testHelper = require('../test-helpers')
+const usersService = require('../../users-service')
 const jestExtended = require('jest-extended')
 
 
@@ -11,9 +12,7 @@ const jestExtended = require('jest-extended')
 // 💡 TIP: This is how to achieve this:
 // ⓵ Open your IDE terminal
 // ⓶ Run the command 'npm run test:dev'
-// ⓷ Within the terminal, type the letter 'p', this allows you to filter tests by the file name. Now type this file name and click 'Enter'
-// 💡Another option - If you prefer not to run tests in Watch mode, just type 'npm test -- mission'
-
+// ⓷ Within the terminal, type the letter 'p', this allows you to filter tests by he file name. Now type this file name and click 'Enter'
 
 test('👶🏽 This is a playground test 🚂', () => {
     expect(true).toBe(true);
@@ -24,22 +23,69 @@ test('👶🏽 This is a playground test 🚂', () => {
 // 💡 TIP: This the syntax of describe: describe('', () => {//test comes here});
 
 // ✅ TASK: Add your first test 🎉. This test should not test real code, just choose a title and put one hard-coded assertion (expect)
-// 💡 TIP: At minimum your assertion might look like: expect(true).toBe(true)
-// 💡 Try multiple assertions type to get familiar with the expect API
-
-
 
 // ✅ TASK: Test the 'validateUser' method of the 'usersService': Ensure that when no 'name' property is provided, 
 //    the response 'succeeded' property is false. Remember the fail-first principle, ensure the test fail when appropriate
 // 💡 TIP: Here's a valid user object to pass. Remove the property name from this object.
-
-
-const userExample = {
+const user = {
     name: 'Kent',
     familyName: 'Beck',
     zipCode: '32486-01',
     address: 'Moonlight road 181, Alaska'
 };
+
+test('When no name is provided, the validation fails', () => {
+    /// Arrange
+    const userServiceUnderTest = new usersService();
+    const userWithNoName = {
+        name: undefined,
+        familyName: 'Beck',
+        zipCode: '32486-01',
+        address: 'Moonlight road 181, Alaska'
+    };
+
+    // Act
+    const receivedResponse = userServiceUnderTest.validateUser(userWithNoName);
+
+
+    // Assert
+    expect(receivedResponse.succeeded).toBe(false);
+});
+
+test('When all properties are valid, then the response should be positive', () => {
+    const userServiceUnderTest = new usersService();
+    const aValidUser = {
+        name: "Joe",
+        familyName: 'Beck',
+        zipCode: '32486-01',
+        address: 'Moonlight road 181, Alaska'
+    };
+
+    // Act
+    const receivedResponse = userServiceUnderTest.validateUser(aValidUser);
+
+    // Assert
+    expect(receivedResponse.succeeded).toBe(true);
+    expect(receivedResponse.reasons.length).toBe(0);
+});
+
+test('When multiple properties are missing, then the response should specify ALL of them', () => {
+    const userServiceUnderTest = new usersService();
+    const aValidUser = {
+        name: undefined,
+        familyName: 'Beck',
+        zipCode: undefined,
+        address: undefined
+    };
+
+    // Act
+    const receivedResponse = userServiceUnderTest.validateUser(aValidUser);
+
+    // Assert
+    expect(receivedResponse.succeeded).toBe(false);
+    expect(receivedResponse.reasons).toIncludeAllMembers(['no-location', 'no-name'])
+
+});
 
 
 
@@ -59,13 +105,18 @@ const userExample = {
 
 // ✅ TASK: Create another test against the 'validateUser' method of the 'usersService':
 //    Provide all the mandatory field and ensure that the response is satisfactory (as expected)
-// 💡 TIP: The response contains two relevant fields to check upon, check both. Use a single assertion to check both fields!
+// 💡 TIP: The response contains two relevant fields to check upon, check both
 
 // ✅ TASK: Create another test against the 'validateUser' method of the 'usersService':
 //    Don't pass at least two mandatory properties, and ensure that the response.succeeded is false
-//    but also that the 'reasons' array has ALL the right reasons inside
+//    but also that the 'reasons' array has all the right reasons inside
 // 💡 TIP: The npm package jest-extended' has a nice assertion method, '.toIncludeAllMembers([members])', that might help here: 
 //    https://github.com/jest-community/jest-extended#toincludeallmembersmembers
+
+// ✅ TASK: Create another test against the 'validateUser' method of the 'usersService':
+//    Don't pass anything to the function and ensure it throws back an exception
+// 💡 TIP: The expect assertion expect(someMethod).toThrow can be helpful:
+//    https://jestjs.io/docs/en/expect#tothrowerror
 
 // ✅ TASK: Create another test against the 'validateUser' method of the 'usersService':
 //    Don't pass anything to the function and ensure it throws back an exception
@@ -76,16 +127,52 @@ const userExample = {
 //    Pass a valid input and expect a positive response back
 // 💡 TIP: The code under test is an async method, your test must await for a response
 
+test('When adding a new user, then get back a positive result', async () => {
+    /// Arrange
+    const userToAdd = {
+        name: 'Kent',
+        familyName: 'Beck',
+        zipCode: '32486-01',
+        address: 'Moonlight road 181, Alaska'
+    };
+    const userServiceUnderTest = new usersService();
+
+
+    // Act
+    const receivedResponse = await userServiceUnderTest.addNewUser(userToAdd);
+
+    // Assert
+    expect(receivedResponse).toBe(true)
+});
+
 // ✅ TASK: Create the first test against the 'deleteUser' method of the 'usersService':
 //    ensure than a user is deleted the response is positive
 // 💡 TIP: This method returns a callback, hence the test must accept a 'done' function as a param and then
 //    it should call done() right after the assertion line
 //    Code example: test('title', (done) => {done()});
 
+test('When an existing user is deleted, then we get a successful response', async (done) => {
+    /// Arrange
+    const userServiceUnderTest = new usersService();
+    const aValidUser = {
+        name: 'kent',
+        familyName: 'Beck',
+        zipCode: "01010"
+    };
+    await userServiceUnderTest.addNewUser(aValidUser);
+
+    // Act
+    userServiceUnderTest.deleteUser('Kent', (error, response) => {
+        // Assert
+        expect(response.succeed).toBe(true);
+        done();
+    })
+
+});
+
 
 //🎖 CONGRATS. You have internalized the anatomy of software testing. This knowledge can be replicated to write many type
-//of tests including advanced frontend and backend testing. You now have the weapon to move faster, refactor more,
-//and generate great docs! Nota that there is much left to learn 📗
+//of tests including advanced frontend and backend testing 
 //🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 //💜💜💜💜💜💜💜💜💜💜💜
 //🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
