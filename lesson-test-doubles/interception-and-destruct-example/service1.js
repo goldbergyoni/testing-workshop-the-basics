d560ac8d2ae7bba94093fc5d9a26d25888db7c53
Yoni Goldberg <me@goldbergyoni.com>
2023-06-01 13:42:16 +0300

    Genric message (#22)
    * Genric message
    
    * Genric message
    
    ---------
    
    Co-authored-by: Yoni Goldberg <goldbergyoni@Yonis-MacBook-Pro-2.local>

--- lesson-test-doubles/interception-and-destruct-example/service1.js
+++ lesson-test-doubles/interception-and-destruct-example/service1.js
@@ -1,11 +1,12 @@
-module.exports = () => {
-  return "Production message";
+module.exports = {
+  print: () => {
+    return "Production message";
+  },
 };
 
-// module.exports = {
-//   print: () => {
-//     return "Production message";
-//   },
+
+
+
+// module.exports = () => {
+//   return "Production message";
 // };
-
-
